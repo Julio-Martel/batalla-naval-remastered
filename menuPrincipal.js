a074f22b56baababee85a8014572c6f9e7ec0840
Diocleniax789 <julioramdom123@gmail.com>
2025-05-11 20:34:49 -0300

    se han implementado la funcion para la nueva generacion del contenido

--- menuPrincipal.js
+++ menuPrincipal.js
@@ -1,3 +1,15 @@
 export const generarMenuPrincipal = (contenido) => {
-	contenido.innerHTML = 'holaaaa';
+	contenido.innerHTML = `
+		<div class="options">
+			<img src="./images/logo.png" class="logo">
+			<button class="boton-opcion" id="unSoloJugador">Un solo jugador</button>
+			<button class="boton-opcion" id="dosJugadores">Dos jugadores</button>
+			<button class="boton-opcion">Datos historicos</button>
+		</div>
+	`;
+
+	const botonUnSoloJugador = document.getElementById('unSoloJugador');
+	const botonDosJugadores = document.getElementById('dosJugadores');
+
+
 }
