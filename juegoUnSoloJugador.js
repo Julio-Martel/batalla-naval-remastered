45c469a6ab1f002e64aebf5f7b4608f7015cccc5
Diocleniax789 <julioramdom123@gmail.com>
2025-05-11 22:21:09 -0300

    se han agregado las primeras variables para el contenido de introduccion

--- juegoUnSoloJugador.js
+++ juegoUnSoloJugador.js
@@ -1,3 +1,6 @@
-const generarJuegoUnSoloJugador = () => {
-	
+const generarJuegoUnSoloJugador = (contenido) => {
+	const presentacion = document.createElement('div');
+	contenido.appendChild(presentacion);
+	presentacion.classList.add('contenido-introduccion');
+
 }
