49c664f99cea4af880e7e2ce3cc28bc1adb8aaef
Diocleniax789 <julioramdom123@gmail.com>
2025-05-12 22:03:43 -0300

    se agrego un nuevo contenedor para la imagen y el texto

--- juegoUnSoloJugador.js
+++ juegoUnSoloJugador.js
@@ -1,7 +1,9 @@
 export const generarJuegoUnSoloJugador = async(contenido) => {
 	const presentacion = document.createElement('div');
 	const titutuloIntroduccion = document.createElement('h1');
-	
+	const imagenIntroduccion = document.createElement('img');
+	const textoImagen = document.createElement('div');
+
 	const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 	contenido.innerHTML = ``;
@@ -13,9 +15,16 @@ export const generarJuegoUnSoloJugador = async(contenido) => {
 	titutuloIntroduccion.classList.add('titulo-introduccion');
 	titutuloIntroduccion.textContent = 'Introduccion';
 
-	await delay(1500);
+	
+
+	//presentacion.appendChild(imagenIntroduccion);
+	//imagenIntroduccion.classList.add('imagen-introduccion');
+	//imagenIntroduccion.src = './images/introduccion.jpg'
+
+
+	await delay(1000);
 
 	presentacion.classList.add('mostrar-contenido');
 
-	
+
 }
