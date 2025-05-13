a7f38c9a2f68f0f120c311bd95aa5db49ff8a0b2
Diocleniax789 <julioramdom123@gmail.com>
2025-05-12 21:31:52 -0300

    se ha agregado correctamente la transicion para la carga gradual de contenido

--- juegoUnSoloJugador.js
+++ juegoUnSoloJugador.js
@@ -1,6 +1,21 @@
-const generarJuegoUnSoloJugador = (contenido) => {
+export const generarJuegoUnSoloJugador = async(contenido) => {
 	const presentacion = document.createElement('div');
-	contenido.appendChild(presentacion);
-	presentacion.classList.add('contenido-introduccion');
+	const titutuloIntroduccion = document.createElement('h1');
+	
+	const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+	contenido.innerHTML = ``;
+	contenido.appendChild(presentacion);
+
+	presentacion.classList.add('contenido-introduccion');
+	presentacion.appendChild(titutuloIntroduccion);
+		
+	titutuloIntroduccion.classList.add('titulo-introduccion');
+	titutuloIntroduccion.textContent = 'Introduccion';
+
+	await delay(1500);
+
+	presentacion.classList.add('mostrar-contenido');
+
+	
 }
