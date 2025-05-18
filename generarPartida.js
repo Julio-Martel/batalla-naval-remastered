c5c469c2b14c15ad8b91e2858fe1b51733e29088
Diocleniax789 <julioramdom123@gmail.com>
2025-05-17 23:17:41 -0300

    se han creado los nodos para el nuevo contenido de la confuracion del tablero

--- generarPartida.js
+++ generarPartida.js
@@ -1,40 +1,34 @@
+const delay = (ms) => {
+	return new Promise(resolve => setTimeout(resolve,ms));
+}
+
 export const generarPartidaParaUnSoloJugador = async(bandoSeleccionado,contenido) => {
 	if (bandoSeleccionado === 'a') {
-		const contenidoBandoNazi = `
-			<div class="contenido-configuracion-tablero">
-				<h1>Configura tu tablero</h1>
-				<div class="seleccion-barcos-tablero">
-					<div class="seccion-barcos">
-					</div>
-					<div class="tablero">
-						<div class="casilla" id="casilla-vacia">
-						</div>
-						<div class="casilla" id="0-1">
-						</div>
-						<div class="casilla" id="0-2">
-						</div>
-						<div class="casilla" id="0-3">
-						</div>
-						<div class="casilla" id="0-4">
-						</div>
-						<div class="casilla" id="0-5">
-						</div>					
-						<div class="casilla" id="0-6">
-						</div>
-						<div class="casilla" id="0-7">
-						</div>
-						<div class="casilla" id="0-8">
-						</div>
-						<div class="casilla" id="0-9">
-						</div>
-						<div class="casilla" id="0-10">
-						</div>			
-					</div>
-				</div>
-			</div>
-		`;	
-
+		const contenidoBandoNazi = document.createElement('div');
+		const tituloConfigurar = document.createElement('h1');
+		const contenidoBarcosYTablero = document.createElement('div');
+		const botonFinalizarConfiguracion = document.createElement('button');
+		
 		contenido.innerHTML = '';
+		contenido.appendChild(contenidoBandoNazi);
+
+		contenidoBandoNazi.appendChild(tituloConfigurar);
+		contenidoBandoNazi.appendChild(contenidoBarcosYTablero);
+		contenidoBandoNazi.appendChild(botonFinalizarConfiguracion);
+
+		tituloConfigurar.classList.add('titulo-introduccion');
+		tituloConfigurar.textContent = `Prepara tu flota!`;
+
+		contenidoBarcosYTablero.classList.add('seleccion-barcos-tablero');
+		botonFinalizarConfiguracion.classList.add('button');
+
+		botonFinalizarConfiguracion.textContent = `Al campo de batalla!`;
+
+		contenidoBandoNazi.classList.add('contenido-bando-nazi','ocultar-contenido-opacado');
+
+		await delay(1000);
+
+		contenidoBandoNazi.classList.add('mostrar-contenido-opacado');
 
 
 	} else {
