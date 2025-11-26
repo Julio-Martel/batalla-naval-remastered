61f7a629e065b1dc2d05ab0df8aa840a5470b06d
Julio-Martel <julioramdom123@gmail.com>
2025-11-26 20:48:42 -0300

    se ha modificado el import

--- menuPrincipal.js
+++ menuPrincipal.js
@@ -1,11 +1,13 @@
-import { generarJuegoUnSoloJugador } from './juegoUnSoloJugador.js';
+//import { generarJuegoUnSoloJugador } from './juegoUnSoloJugador.js';
+
+import {prepararTableroParaModoDosJugadores} from '.prepararTableroParaModoDosJugadores.js';
 
 export const generarMenuPrincipal = (contenido) => {
 
 	contenido.innerHTML = `
 		<div class="options">
 			<img src="./images/logo.png" class="logo">
-			<button class="boton-opcion" id="unSoloJugador">Un solo jugador</button>
+			<button class="boton-opcion" id="unSoloJugador">Un solo jugador(no disponible)</button>
 			<button class="boton-opcion" id="dosJugadores">Dos jugadores</button>
 			<button class="boton-opcion">Datos historicos</button>
 		</div>
@@ -14,5 +16,5 @@ export const generarMenuPrincipal = (contenido) => {
 	//const botonUnSoloJugador = document.getElementById('unSoloJugador');
 	const botonDosJugadores = document.getElementById('dosJugadores');
 
-	botonDosJugadores.addEventListener('click', () => generarJuegoUnSoloJugador(contenido));
+	botonDosJugadores.addEventListener('click', () => prepararTableroParaModoDosJugadores(contenido));
 }
