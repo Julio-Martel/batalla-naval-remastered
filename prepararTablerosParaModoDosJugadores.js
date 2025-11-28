7a598f19c9981b15c5fda867463fd73256d61073
Julio-Martel <julioramdom123@gmail.com>
2025-11-28 19:40:49 -0300

    se configuro bien la parte del css para el js

--- prepararTablerosParaModoDosJugadores.js
+++ prepararTablerosParaModoDosJugadores.js
@@ -1,6 +1,22 @@
-export const mostrar = () => {  
-
+export const mostrar = async(contenido) => {  
     
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    
+    let elementosContenido = {
+        contenedorPrincipal: document.createElement('div'),
+        tituloDeNroDeJugador: document.createElement('h1'),
+    };  
+   
+    contenido.innerHTML = ``;
+    contenido.appendChild(elementosContenido.contenedorPrincipal);
+   
+    await delay(1000);  
+
+    elementosContenido.contenedorPrincipal.classList.add('contenido-principal');
+    elementosContenido.contenedorPrincipal.classList.add('mostrar-contenido')
+
+    elementosContenido.tituloDeNroDeJugador.textContent = "Jugador Nro 1";
+    elementosContenido.contenedorPrincipal.appendChild(elementosContenido.tituloDeNroDeJugador);
 
 
 }
