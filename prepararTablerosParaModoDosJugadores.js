53f5a4ce1fa7d392248be9e4aa0b5bb0000eaaf6
Julio-Martel <julioramdom123@gmail.com>
2025-11-28 21:56:58 -0300

    se agrego una matriz

--- prepararTablerosParaModoDosJugadores.js
+++ prepararTablerosParaModoDosJugadores.js
@@ -1,10 +1,22 @@
 export const mostrar = async(contenido) => {  
     
     const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-    
+
+    let tableroMatriz = [];
+
+    for(let i = 0; i < 11; i++) {
+        let fila = [];
+        tableroMatriz.push(fila);
+        for(let j = 0; j < 11; j++){
+            tableroMatriz[i][j] = 0;
+        }
+    }
+
     let elementosContenido = {
         contenedorPrincipal: document.createElement('div'),
         tituloDeNroDeJugador: document.createElement('h1'),
+        contenedorBarcosYTablero: document.createElement('div'), 
+    
     };  
    
     contenido.innerHTML = ``;
@@ -18,6 +30,10 @@ export const mostrar = async(contenido) => {
 
     elementosContenido.tituloDeNroDeJugador.textContent = "Jugador Nro 1";
     elementosContenido.contenedorPrincipal.appendChild(elementosContenido.tituloDeNroDeJugador);
+     
+    elementosContenido.contenedorPrincipal.appendChild(elementosContenido.contenedorBarcosYTablero);
+    elementosContenido.contenedorBarcosYTablero.classList.add('contenido-barcos-tablero');
 
+    
 
 }
