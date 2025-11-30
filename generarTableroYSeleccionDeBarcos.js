965e82f6c3f1ba1a92707691f7a3529593ddf0fa
Julio-Martel <julioramdom123@gmail.com>
2025-11-29 21:00:42 -0300

    se han agregado las primeras varibles para la generacion del contenido tablero y seleccion de barcos

---
+++ generarTableroYSeleccionDeBarcos.js
@@ -0,0 +1,11 @@
+export const generarTableroYBarcos = (contenidoPrincipal,nroDeJugador) => {
+    let tituloDelJugador = document.createElement('h1');
+    let contenedorTableroBarcos = document.createElement('div');
+    let seccionBarcosParaSeleccionar = document.createElement('div');
+    let tablero = document.createElement('div');
+    let botonParaFinalizarLaCargaConfiguracionDeTablero = document.createElement('button');
+
+
+
+
+}
